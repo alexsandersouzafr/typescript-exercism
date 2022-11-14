8206c7064a6df6abd7ba73088223e65dee2bc737
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2022-11-14 17:19:58 -0300

    matrix unfinished

--- matrix/matrix.ts
+++ matrix/matrix.ts
@@ -1,13 +1,35 @@
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  numberString: string;
+
+  constructor(numbers: string) {
+    this.numberString = numbers;
   }
 
-  get rows(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get rows(): Array<number[]> {
+    let mountMatrix = (str: string): Array<number[]> => {
+      let matrix: Array<number[]> = [[]];
+      let row = 0;
+      let numbers: string[] = [...str];
+      numbers.forEach((char, i) => {
+        switch (true) {
+          //TODO: concatenar números entre espaços antes de testar
+          case /[0-9]/.test(char):
+            matrix[row].push(parseInt(char));
+            break;
+          case /\n/.test(char):
+            row++;
+            matrix.push([]);
+            break;
+          default:
+            null;
+        }
+      });
+      return matrix;
+    };
+    return mountMatrix(this.numberString);
   }
 
-  get columns(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get columns(): number[] {
+    return [0];
   }
 }
